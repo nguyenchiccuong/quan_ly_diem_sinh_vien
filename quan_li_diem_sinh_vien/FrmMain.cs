2c03f528ded03c51643a6f7b307d927efc1c74fa
CuongNC <nccuong281299@gmail.com>
2021-11-07 10:22:22 +0700

    frm main able to call frm sinh vien dki

--- quan_li_diem_sinh_vien/FrmMain.cs
+++ quan_li_diem_sinh_vien/FrmMain.cs
@@ -139,6 +139,18 @@ namespace quan_li_diem_sinh_vien
                 f.Show();
             }
         }
+
+        private void barBtnDangKy_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Form frm = this.CheckExists(typeof(FrmSinhVienDki));
+            if (frm != null) frm.Activate();
+            else
+            {
+                FrmSinhVienDki f = new FrmSinhVienDki();
+                f.MdiParent = this;
+                f.Show();
+            }
+        }
     }
 
 
