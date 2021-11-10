025dd58e07fb27e76967abe14ba63a0d3f513c11
CuongNC <nccuong281299@gmail.com>
2021-11-10 23:37:59 +0700

    frm sinh vien dki combo box able to change

--- quan_li_diem_sinh_vien/FrmSinhVienDki.cs
+++ quan_li_diem_sinh_vien/FrmSinhVienDki.cs
@@ -20,10 +20,6 @@ namespace quan_li_diem_sinh_vien
 
         private void FrmSinhVienDki_Load(object sender, EventArgs e)
         {
-            DSSVDKC2.EnforceConstraints = false;
-            this.dangKyTableAdapter2.Connection.ConnectionString = Program.connstr;
-            this.dangKyTableAdapter2.Fill(this.DSSVDKC2.DANG_KI);
-
             layDSNKHK("SELECT * FROM V_DS_NK_HK");
 
             DSSVDKC.EnforceConstraints = false;
@@ -46,6 +42,11 @@ namespace quan_li_diem_sinh_vien
                 int indexOfMonHoc = monHocBDS.Find("MA_MH", ((DataRowView)lopTinChiBDS[viTriLop])["MA_MH"].ToString());
                 lblTenMonHoc1.Text = ((DataRowView)monHocBDS[indexOfMonHoc])["TEN_MH"].ToString();
             }
+            else
+            {
+                lblTenMonHoc1.Text = "";
+            }
+
             layDSGV("SELECT * FROM V_DS_GV");
             if (giangBDS.Count > 0)
             {
@@ -54,7 +55,46 @@ namespace quan_li_diem_sinh_vien
                 lblTenGiangVien.Text = ((DataRowView)giangVienBDS[indexOfGiangVien])["HO_TEN"].ToString();
                 lblSoLuongSinhVienDaDki.Text = dangKyBDS.Count.ToString();
             }
+            else
+            {
+                lblTenGiangVien.Text = "";
+                lblSoLuongSinhVienDaDki.Text = "";
+            }
 
+            DSSVDKC2.EnforceConstraints = false;
+            this.dangKyTableAdapter2.Connection.ConnectionString = Program.connstr;
+            this.dangKyTableAdapter2.Fill(this.DSSVDKC2.DANG_KI);
+            if (dangKyBDS2.Count > 0)
+            {
+                dangKyBDS2.Filter = "MA_SV = '" + Program.mloginDN + "'";
+                for (int i = 0; i < lopTinChiBDS.Count; i++)
+                {
+                    String maLopTc = ((DataRowView)lopTinChiBDS[i])["MA_LOP_TC"].ToString();
+                    dangKyBDS2.Filter += " AND MA_LOP_TC = " + maLopTc;
+                }
+            }
+            this.dangKyTableAdapter2.Fill(this.DSSVDKC2.DANG_KI);
+
+            if (dangKyBDS2.Count > 0)
+            {
+                int maLopTc = int.Parse(((DataRowView)dangKyBDS2[dangKyBDS2.Position])["MA_LOP_TC"].ToString());
+                int indexOfLopTc = lopTinChiBDS.Find("MA_LOP_TC", maLopTc);
+                int indexOfMonHoc = monHocBDS.Find("MA_MH", ((DataRowView)lopTinChiBDS[indexOfLopTc])["MA_MH"].ToString());
+                lblTenMonHoc2.Text = ((DataRowView)monHocBDS[indexOfMonHoc])["TEN_MH"].ToString();
+            }
+            else
+            {
+                lblTenMonHoc2.Text = "";
+            }
+
+            if (kiemTraChoPhepHieuChinh() != 0)
+            {
+                barBtnXoa.Enabled = barBtnDangKi.Enabled = false;
+            }
+            else
+            {
+                barBtnXoa.Enabled = barBtnDangKi.Enabled = true;
+            }
         }
 
         private void layDSNKHK(String cmd)
@@ -118,41 +158,104 @@ namespace quan_li_diem_sinh_vien
                 if (nienKhoaHocKyBDS.Count > 0)
                 {
                     lopTinChiBDS.Filter = "MA_NK_HK = " + cboNKHK.SelectedValue + "AND HUY = false";
-                    this.lopTinChiTableAdapter.Fill(this.DSSVDKC.LOP_TIN_CHI);
+
                 }
+                this.lopTinChiTableAdapter.Fill(this.DSSVDKC.LOP_TIN_CHI);
+                this.dangKyTableAdapter.Fill(this.DSSVDKC.DANG_KI);
+                this.giangTableAdapter.Fill(this.DSSVDKC.GIANG);
+
+                if (lopTinChiBDS.Count > 0)
+                {
+                    viTriLop = lopTinChiBDS.Position;
+                    int indexOfMonHoc = monHocBDS.Find("MA_MH", ((DataRowView)lopTinChiBDS[viTriLop])["MA_MH"].ToString());
+                    lblTenMonHoc1.Text = ((DataRowView)monHocBDS[indexOfMonHoc])["TEN_MH"].ToString();
+                }
+                else
+                {
+                    lblTenMonHoc1.Text = "";
+                }
+
+                if (giangBDS.Count > 0)
+                {
+                    viTriGiang = giangBDS.Position;
+                    int indexOfGiangVien = giangVienBDS.Find("MA_GV", ((DataRowView)giangBDS[viTriGiang])["MA_GV"].ToString());
+                    lblTenGiangVien.Text = ((DataRowView)giangVienBDS[indexOfGiangVien])["HO_TEN"].ToString();
+                    lblSoLuongSinhVienDaDki.Text = dangKyBDS.Count.ToString();
+                }
+                else
+                {
+                    lblTenGiangVien.Text = "";
+                    lblSoLuongSinhVienDaDki.Text = "";
+                }
+
+                dangKyBDS2.Filter = "";
+                this.dangKyTableAdapter2.Fill(this.DSSVDKC2.DANG_KI);
+                if (dangKyBDS2.Count > 0)
+                {
+                    dangKyBDS2.Filter = "MA_SV = '" + Program.mloginDN + "'";
+                    for (int i = 0; i < lopTinChiBDS.Count; i++)
+                    {
+                        String maLopTc = ((DataRowView)lopTinChiBDS[i])["MA_LOP_TC"].ToString();
+                        dangKyBDS2.Filter += " AND MA_LOP_TC = " + maLopTc;
+                    }
+                }
+                this.dangKyTableAdapter2.Fill(this.DSSVDKC2.DANG_KI);
+
+                if (dangKyBDS2.Count > 0)
+                {
+                    int maLopTc = int.Parse(((DataRowView)dangKyBDS2[dangKyBDS2.Position])["MA_LOP_TC"].ToString());
+                    int indexOfLopTc = lopTinChiBDS.Find("MA_LOP_TC", maLopTc);
+                    int indexOfMonHoc = monHocBDS.Find("MA_MH", ((DataRowView)lopTinChiBDS[indexOfLopTc])["MA_MH"].ToString());
+                    lblTenMonHoc2.Text = ((DataRowView)monHocBDS[indexOfMonHoc])["TEN_MH"].ToString();
+                }
+                else
+                {
+                    lblTenMonHoc2.Text = "";
+                }
+
+                if (kiemTraChoPhepHieuChinh() != 0)
+                {
+                    barBtnXoa.Enabled = barBtnDangKi.Enabled = false;
+                }
+                else
+                {
+                    barBtnXoa.Enabled = barBtnDangKi.Enabled = true;
+                }
+
             }
             catch (Exception ex)
             {
+                Console.WriteLine(ex);
             }
 
         }
 
         private void lopTinChiGridControl_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void giangGridControl_Click(object sender, EventArgs e)
-        {
             if (lopTinChiBDS.Count > 0)
             {
                 viTriLop = lopTinChiBDS.Position;
                 int indexOfMonHoc = monHocBDS.Find("MA_MH", ((DataRowView)lopTinChiBDS[viTriLop])["MA_MH"].ToString());
                 lblTenMonHoc1.Text = ((DataRowView)monHocBDS[indexOfMonHoc])["TEN_MH"].ToString();
+                lblSoLuongSinhVienDaDki.Text = dangKyBDS.Count.ToString();
             }
+        }
+
+        private void giangGridControl_Click(object sender, EventArgs e)
+        {
             if (giangBDS.Count > 0)
             {
                 viTriGiang = giangBDS.Position;
                 int indexOfGiangVien = giangVienBDS.Find("MA_GV", ((DataRowView)giangBDS[viTriGiang])["MA_GV"].ToString());
                 lblTenGiangVien.Text = ((DataRowView)giangVienBDS[indexOfGiangVien])["HO_TEN"].ToString();
-                lblSoLuongSinhVienDaDki.Text = dangKyBDS.Count.ToString();
             }
         }
 
         private int kiemTraChoPhepHieuChinh()
         {
-            DateTime ngayBatDau = Convert.ToDateTime(((DataRowView)nienKhoaHocKyBDS[nienKhoaHocKyBDS.Position])["NGAY_BDAU_DKI"].ToString());
-            DateTime ngayKetThuc = Convert.ToDateTime(((DataRowView)nienKhoaHocKyBDS[nienKhoaHocKyBDS.Position])["NGAY_KTHUC_DKI"].ToString());
+            int indexOfNKHK = nienKhoaHocKyBDS.Find("MA_NK_HK", cboNKHK.SelectedValue);
+            DateTime ngayBatDau = Convert.ToDateTime(((DataRowView)nienKhoaHocKyBDS[indexOfNKHK])["NGAY_BDAU_DKI"].ToString());
+            DateTime ngayKetThuc = Convert.ToDateTime(((DataRowView)nienKhoaHocKyBDS[indexOfNKHK])["NGAY_KTHUC_DKI"].ToString());
             if ((DateTime.Compare(DateTime.Now, ngayBatDau) < 0))
             {
                 return -1;
